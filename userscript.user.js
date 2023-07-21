54330ab0d15936de999315b9425f64c9578476bc
antonio32a <~@antonio32a.com>
2023-07-21 20:43:32 +0200

    Bump version

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place Hypixel Overlay
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  r/place overlay with an autoplacer.
 // @author       Antonio32A
 // @credits      oralekin, exdeejay (xDJ_), 101arrowz
